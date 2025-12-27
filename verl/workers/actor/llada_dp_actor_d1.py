d35e89c4bf0f47ccbeddd60365f74be7555a007e
yjyddq <1004328545@qq.com>
2025-12-28 03:35:45 +0800

    fix error/bug in dp_actor_d1

--- verl/workers/actor/llada_dp_actor_d1.py
+++ verl/workers/actor/llada_dp_actor_d1.py
@@ -90,8 +90,8 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
             seq = micro_batch["input_ids"]  # (bs, seq_len)
             attention_mask = micro_batch["attention_mask"]  # (bs, mc_num, seq_len)
 
+            loss_per_sample = torch.zeros((batch_size, mc_num), device=device)
             log_prob = torch.zeros((batch_size, mc_num, response_length), device=device)
-            elbo_per_sample = torch.zeros((batch_size, mc_num), device=device)
             for i in range(mc_num):
                 cur_perturbed_seq = perturbed_seq[:, i, :]  # (batch_size, seq_len)
                 cur_mask_indices = mask_indices[:, i, :]
@@ -111,7 +111,6 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
                 logits = self._get_logits(model=self.actor_module, packed_input=packed_perturbed_seq, cu_seqlens=cu_seqlens, max_seqlen=max_seqlen, prompt_len=attention_mask[:, :prompt_length].sum(dim=1), cfg_scale=0.0, MASK_TOKEN_ID=self.MASK_TOKEN_ID)
                 
                 # Restore logits for each sample
-                # loss = torch.zeros((batch_size, response_length), device=device)
                 for b in range(batch_size):
                     start, end = cu_seqlens[b], cu_seqlens[b + 1]
                     
@@ -119,18 +118,18 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
                     logits_b[attention_mask[b] == 1] = logits[0, start:end]
                     
                     mask = cur_mask_indices[b]  # (seq_len,)
-                    log_prob[b, i, :] = F.cross_entropy(logits_b, seq[b], reduction="none")[-response_length:] # (response_length,)
-                    elbo_per_sample[b, i] = - (F.cross_entropy(logits_b[mask], seq[b][mask], reduction="none") / cur_p_mask[b][mask]).sum()  
+                    log_prob[b, i, :] = - (F.cross_entropy(logits_b, seq[b], reduction="none")[-response_length:]) # (response_length,)
+                    loss_per_sample[b, i] = - (F.cross_entropy(logits_b[mask], seq[b][mask], reduction="none") / cur_p_mask[b][mask]).sum()  
                     # cross_entropy returns negative log likelihood, - cross_entropy convert it to elbo
 
-            elbo = (elbo_per_sample / response_length).unsqueeze(-1).expand(-1, -1, response_length).contiguous() # (batch_size, mc_num, response_length)
+            loss_per_sample = (loss_per_sample / response_length).unsqueeze(-1).expand(-1, -1, response_length).contiguous() # (batch_size, mc_num, response_length)
 
         entropy = None
         if calculate_entropy:
             probs = log_prob.exp()
-            entropy = -probs * log_prob  # (bs, response_length) entropy of each token
+            entropy = -probs * log_prob  # (bs, mc_num, response_length) entropy of each token
             
-        return entropy, log_prob, elbo
+        return entropy, log_prob, loss_per_sample
     
     def _get_logits(self, model, packed_input, cu_seqlens, max_seqlen, prompt_len, cfg_scale=0.0, MASK_TOKEN_ID=126336):
         """
@@ -197,19 +196,19 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
 
         log_prob_lst = []
         entropy_lst = []
-        elbo_lst = []
+        loss_per_sample_lst = []
         for micro_batch in micro_batches:
             if isinstance(micro_batch, DataProto):
                 micro_batch = {**micro_batch.batch, **micro_batch.non_tensor_batch}
             with torch.no_grad():
-                entropy, log_prob, elbo = self._forward_micro_batch(micro_batch, temperature=temperature, n_l=self.n_l, mc_num=self.mc_num, calculate_entropy=calculate_entropy, call_fn_name="compute_log_prob")
+                entropy, log_prob, loss_per_sample = self._forward_micro_batch(micro_batch, temperature=temperature, n_l=self.n_l, mc_num=self.mc_num, calculate_entropy=calculate_entropy, call_fn_name="compute_log_prob")
             log_prob_lst.append(log_prob)
-            elbo_lst.append(elbo)
+            loss_per_sample_lst.append(loss_per_sample)
             if calculate_entropy:
                 entropy_lst.append(entropy)
 
         log_probs = torch.concat(log_prob_lst, dim=0)
-        elbo = torch.concat(elbo_lst, dim=0)
+        loss_per_sample = torch.concat(loss_per_sample_lst, dim=0)
         entropys = None
         if calculate_entropy:
             entropys = torch.concat(entropy_lst, dim=0)
@@ -218,8 +217,8 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
             assert len(indices) == log_probs.size(0), f"{len(indices)} vs. {log_probs.size()}"
             revert_indices = torch.tensor(get_reverse_idx(indices), dtype=torch.long)
             log_probs = log_probs[revert_indices]
-            elbo = elbo[revert_indices]
-        return entropys, log_probs, elbo
+            loss_per_sample = loss_per_sample[revert_indices]
+        return entropys, log_probs, loss_per_sample
 
     @GPUMemoryLogger(role="dp actor", logger=logger)
     def update_policy(self, data: DataProto):
@@ -229,11 +228,11 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
         temperature = data.meta_info["temperature"]  # temperature must be in the data.meta_info to avoid silent error
         multi_turn = data.meta_info.get("multi_turn", False)
 
-        select_keys = ["responses", "input_ids", "attention_mask", "position_ids", "old_log_probs", "old_elbo", "advantages", "perturbed_seq", "mask_indices", "p_mask"]
+        select_keys = ["responses", "input_ids", "attention_mask", "position_ids", "old_log_probs", "old_loss_per_sample", "advantages", "perturbed_seq", "mask_indices", "p_mask"]
         if multi_turn:
             select_keys.append("loss_mask")
         if self.config.use_kl_loss:
-            select_keys.append("ref_log_prob")
+            select_keys.append("ref_log_probs")
         batch = data.select(batch_keys=select_keys).batch
         has_multi_modal_inputs = "multi_modal_inputs" in data.non_tensor_batch.keys()
 
@@ -280,6 +279,7 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
                         response_mask = attention_mask[:, -response_length:]
 
                     old_log_probs = data["old_log_probs"]  # (bsz, mc_num, response_length)
+                    old_loss_per_sample = data["old_loss_per_sample"]  # (bsz, mc_num, response_length)
                     advantages = data["advantages"]
 
                     clip_ratio = self.config.clip_ratio
@@ -312,15 +312,15 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
                             "mask_indices": mask_indices[:, i:i+1],
                             "p_mask": p_mask[:, i:i+1],
                         }
-                        entropy, log_prob, elbo = self._forward_micro_batch(micro_batch=data, temperature=temperature, n_l=1, mc_num=1, calculate_entropy=calculate_entropy, call_fn_name="update_policy")
-                        print(f"\nelbo: {elbo[0, 0, 0]}")
+                        entropy, log_prob, loss_per_sample = self._forward_micro_batch(micro_batch=data, temperature=temperature, n_l=1, mc_num=1, calculate_entropy=calculate_entropy, call_fn_name="update_policy")
+                        print(f"\nloss_per_sample: {loss_per_sample[0, 0, 0]}")
                         
                         # Compute policy loss
                         pg_loss, pg_clipfrac, ppo_kl, pg_clipfrac_lower = compute_policy_loss(
                             old_l_theta=old_log_probs[:, i, :],  # (bsz, response_length)
                             l_theta=log_prob[:, 0, :],  # (bsz, response_length)
                             advantages=advantages,
-                            response_mask=mask_indices[:, i, :][:, -response_length:],
+                            response_mask=mask_indices[:, i, :][:, -response_length:],  # (bsz, response_length)
                             cliprange=clip_ratio,
                             cliprange_low=clip_ratio_low,
                             cliprange_high=clip_ratio_high,
@@ -337,9 +337,9 @@ class DLLMDataParallelPPOActor(DataParallelPPOActor):
                             policy_loss = pg_loss
 
                         if self.config.use_kl_loss:  # NOTE: Currently not considering KL
-                            ref_log_prob = data["ref_log_prob"]
+                            ref_log_probs = data["ref_log_probs"]
                             # compute kl loss
-                            kld = kl_penalty(l_theta=log_prob[:, 0, :], ref_l_theta=ref_log_prob[:, i, :], kl_penalty=self.config.kl_loss_type, advantages=advantages)
+                            kld = kl_penalty(l_theta=log_prob[:, 0, :], ref_l_theta=ref_log_probs[:, i, :], kl_penalty=self.config.kl_loss_type, advantages=advantages)
                             kl_loss = agg_loss(loss_mat=kld, loss_mask=mask_indices[:, i, :][:, -response_length:], loss_agg_mode=loss_agg_mode)
 
                             policy_loss = policy_loss + kl_loss * self.config.kl_loss_coef
